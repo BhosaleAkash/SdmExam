07d8a66ef01b0b079558bb7f2dcd98bdeacdb218
akash <akashbhosale2288@gmail.com>
2023-02-02 15:18:21 +0530

    second commit

--- Test.java
+++ Test.java
@@ -3,11 +3,11 @@ class Test
     public static void main(String args[])
     {
 
-        int n1=0, n2=1, n3,i, count=0;
+        int n1=0, n2=1, n3,i, count=20;
 
         System.out.print(n1 + " " + n2);
 
-        for(i = 0, i , count; i++)
+        for(i = 0, i<=count; i++)
         {
             n3 = n1+ n2;
 
@@ -18,4 +18,19 @@ class Test
             n2= n3;
         }
     }
+
+    //prime number
+    public static void main(String args[])
+    {
+
+        for(int i= 1; i <=20; i++)
+        {
+            if(isPrime(i))
+            {
+                System.out.println(i);
+            }
+        }
+    }
+
+
 }
