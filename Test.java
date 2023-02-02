d449de0ac1cc40f01743ffda5866d38144e687c4
akash <akashbhosale2288@gmail.com>
2023-02-02 15:29:39 +0530

    third commit

--- Test.java
+++ Test.java
@@ -32,5 +32,31 @@ class Test
         }
     }
 
+    //
+    public static void main(String args[])
+    {
+        int numbet 313 ;
+        int orininalNumber , reminder , result=0;
+
+        orininalNumber= reminder;
+
+        while(orininalNumber !=0)
+        {
+            reminder = originalNumber % 10;
+            result += Math.pow(reminder , 2);
+            originalNumber /= 10;
+
+        }
+
+        if (result == number)
+        {
+            System.out.println("Armstrong");
+        }
+
+        else{
+            System.out.println(" not Armstrong");
+        }
+    }
+
 
 }
