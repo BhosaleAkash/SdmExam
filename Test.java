5cddf41c1bcadc7ecacb8ae86bbc55eb01cc0172
akash <akashbhosale2288@gmail.com>
2023-02-02 14:56:39 +0530

    first commit

---
+++ Test.java
@@ -0,0 +1,21 @@
+class Test
+{
+    public static void main(String args[])
+    {
+
+        int n1=0, n2=1, n3,i, count=0;
+
+        System.out.print(n1 + " " + n2);
+
+        for(i = 0, i , count; i++)
+        {
+            n3 = n1+ n2;
+
+            system.out.print(" "+n3);
+
+            n1 = n2;
+
+            n2= n3;
+        }
+    }
+}
